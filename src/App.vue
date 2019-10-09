57eb05fe0b9bc2461970362ece3f00ba9256c7e0
Kevin McDermott <kmcdermott31@gmail.com>
2019-10-09 15:48:40 +0100

    done a bunch of extensions

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>Countries App</h1>
     <div class="page-container">
       <div class="country-list">
-        <countries-list :countries="countries"></countries-list>
+        <country-select :countries="countries"></country-select>
       </div>
       <div class="country-info">
         <country-detail :country="selectedCountry"></country-detail>
@@ -14,8 +14,8 @@
 
 <script>
 import {eventBus} from './main.js';
-import CountriesList from './components/CountriesList.vue';
 import CountryDetail from './components/CountryDetail.vue';
+import CountrySelect from './components/CountrySelect.vue';
 
 export default {
   name: 'app',
@@ -35,25 +35,30 @@ export default {
     })
   },
   components: {
-    "countries-list": CountriesList,
-    "country-detail": CountryDetail
+    "country-detail": CountryDetail,
+    "country-select": CountrySelect
   }
 }
 </script>
 
 <style lang="css" scoped>
+h1 {
+  text-align: center;
+}
+
 .page-container {
   display: flex;
-  flex-flow: wrap;
+  flex-direction: row;
+  justify-content: center;
 }
 
 .country-list {
   flex: 50%;
-  justify-content: center;
+  align-content: center;
 }
 
-.country-detail {
+.country-info {
   flex: 50%;
-  justify-content: center;
+  align-content: center;
 }
 </style>
---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div>
+    <select @change="selectHandler" v-model="country">
+      <option v-for="country in countries" :value="country">{{ country.name }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import {eventBus} from '../main.js';
+
+export default {
+  name: 'country-select',
+  data(){
+    return{
+      country: null
+    }
+  },
+  props: ['countries'],
+  methods: {
+    selectHandler(){
+      eventBus.$emit('country-selected', this.country)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
